fe13a988e508ad6c5381ed32ca3ad057b98864fc
jeonsang-oh <jeon.sangoh92@gmail.com>
2023-02-13 20:49:12 +0900

    chapter1 기본알고리즘 - 반복

--- algorithm-java-chapter/src/main/java/org/chapter01/StarPira.java
+++ algorithm-java-chapter/src/main/java/org/chapter01/StarPira.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class StarPira {
     static void spira(int n) {
         for (int i = 1; i <= n; i++) { 					// i행 (i = 1, 2, … ,n)
-            for (int j = 1; j <= n - i + 1; j++) 		// n-i+1개의 ' '를 나타냄
+            for (int j = 1; j <= n - i + 1; j++) 		// n-i+1개의 ' '(띄어쓰기)를 나타냄
                 System.out.print(' ');
             for (int j = 1; j <= (i - 1) * 2 + 1; j++) 	// (i-1)*2+1개의 '*'를 나타냄
                 System.out.print('*');
