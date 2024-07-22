9a9ab0135f8da0d76aa5768e18cd0618aea3e35e
jeonsang-oh <jeon.sangoh92@gmail.com>
2024-07-23 05:43:46 +0900

    chapter1 기본알고리즘 - 반복

---
+++ algorithm-java-chapter/src/main/java/org/chapter01/SpecialWordPrint.java
@@ -0,0 +1,11 @@
+package org.chapter01;
+
+import java.util.Scanner;
+
+public class SpecialWordPrint {
+
+    public static void main(String[] args) {
+        // 출력 예시 : !@#$%^&*(\'"<>?:;
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
